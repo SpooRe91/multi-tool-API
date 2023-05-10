141b3cd700bb0b0ed6c6a8d4658364faa04cca7c
Martin B <m.bogdanov9110@gmail.com>
2023-05-10 19:25:07 +0300

    removed the require of postgresTest module

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const homePageController = require('./controllers/homePageController');
-const postgresTest = require('./controllers/postgresTest');
+// const postgresTest = require('./controllers/postgresTest');
 const editController = require('./controllers/editController');
 const searchController = require('./controllers/searchController');
 const createController = require('./controllers/createController');
@@ -12,7 +12,7 @@ router.use((req, res, next) => {
     console.log(`ORIGIN: ${req.headers.host} - REQUEST: ${req.method} >> PATH: ${req.path}`);
     next();
 })
-router.use(homePageController, postgresTest, editController);
+router.use(homePageController, /*postgresTest,*/ editController);
 router.use('/auth', authController);
 router.use('/recipe', searchController, createController);
 router.get('*', (req, res) => {
