c253b6e817560303fb89d8054209837316597fbb
Martin B <m.bogdanov9110@gmail.com>
2023-07-26 19:28:17 +0300

    UPDATED cors, express, mngoose

--- src/controllers/destinyController.js
+++ src/controllers/destinyController.js
@@ -2,11 +2,11 @@ const router = require("express").Router();
 require("dotenv").config();
 const axios = require("axios");
 
-const URL = axios.create({ baseURL: "https://www.bungie.net/Platform/" });
+const URL = axios.create({ baseURL: "https://www.bungie.net/Platform" });
 
 router.get("/clanInfo", async (req, res) => {
   try {
-    const result = URL.get("GroupV2/4131725", {
+    const result = URL.get("/GroupV2/4131725", {
       headers: {
         "content-type": "application/json",
         "X-API-Key": process.env.BUNGIE_API_KEY,
@@ -18,16 +18,14 @@ router.get("/clanInfo", async (req, res) => {
     }
   } catch (error) {
     if (error instanceof Error) {
-      res
-        .status(error.status || 400)
-        .json("There is a problem with the request, please try again later!");
+      res.status(error.status || 400).json(error);
     }
   }
 });
 
 router.get("/getClanMembers", async (req, res) => {
   try {
-    const result = URL.get("GroupV2/4131725/Members", {
+    const result = URL.get("/GroupV2/4131725/Members", {
       headers: {
         "content-type": "application/json",
         "X-API-Key": process.env.BUNGIE_API_KEY,
@@ -53,7 +51,7 @@ router.get("/getUser/:id", async (req, res) => {
     if (!id) {
       res.status(404).json("Please enter a valid ID");
     }
-    const result = URL.get(`User/GetBungieNetUserById/${id}`, {
+    const result = URL.get(`/User/GetBungieNetUserById/${id}`, {
       headers: {
         "content-type": "application/json",
         "X-API-Key": process.env.BUNGIE_API_KEY,
@@ -85,7 +83,7 @@ router.get("/getCharacter/:param1/:param2", async (req, res) => {
         .json("Please enter valid membership type and membership ID!");
     }
     const result = URL.get(
-      `Destiny2/${param1}/Account/${param2}/Character/0/Stats`,
+      `/Destiny2/${param1}/Account/${param2}/Character/0/Stats`,
       {
         headers: {
           "content-type": "application/json",
@@ -103,6 +101,25 @@ router.get("/getCharacter/:param1/:param2", async (req, res) => {
       res
         .status(error.status || 400)
         .json("There is a problem with the request, please try again later!");
+    }
+  }
+});
+
+router.get("/news", async (req, res) => {
+  try {
+    const result = URL.get("/Trending/Categories/", {
+      headers: {
+        "content-type": "application/json",
+        "X-API-Key": process.env.BUNGIE_API_KEY,
+      },
+    });
+    const data = await result;
+    if (data.status === 200) {
+      res.status(200).json(data.data);
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(error.status || 400).json(error);
     }
   }
 });
