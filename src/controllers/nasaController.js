aa05a8f14f4c205a8122dd026954095459f6532e
Martin B <m.bogdanov9110@gmail.com>
2023-08-22 20:22:30 +0300

    Added NasaArrticle endpoint

--- src/controllers/nasaController.js
+++ src/controllers/nasaController.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const { getErrorMessage } = require("../utils/errorHelpers");
 require("dotenv").config();
 const axios = require("axios");
 
@@ -21,4 +20,40 @@ router.get("/pod", async (req, res) => {
     }
   }
 });
+
+router.get("/articles/", async (req, res) => {
+  const query = req.query.query;
+  const forbidden = ["javascript", "script", "code", "/", ":", "<", ">", "\\"];
+
+  if (forbidden.includes(query.toLowerCase())) {
+    return res.status(400).json("ERROR: Invalid query search!");
+  }
+
+  try {
+    const result = axios
+      .create({ baseURL: "https://api.spaceflightnewsapi.net/" })
+      .get(
+        `/v4/articles/?limit=20&ordering=updated_at&search=${query.toLowerCase()}`
+      );
+
+    const data = await result;
+
+    if (data.data.count !== 0 && data.status >= 200 && data.status <= 300) {
+      return res.status(data.status || 200).json(data.data);
+    }
+
+    if (data.data.count === 0) {
+      return res
+        .status(data.status || 200)
+        .json("Sorry, there are no results!");
+    }
+  } catch (error) {
+    if (typeof error === "string") {
+      res.status(data.status || 400).json({ message: error.toUpperCase() });
+    } else if (error instanceof Error) {
+      res.status(data.status || 400).json(error.message);
+    }
+  }
+});
+
 module.exports = router;
