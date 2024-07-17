1b876003af285035c69875a4e5aecc98a9447ea8
Martin B <m.bogdanov9110@gmail.com>
2024-07-17 18:26:15 +0300

    Added users task to the origins

--- src/index.js
+++ src/index.js
@@ -19,33 +19,32 @@ dbService.connecter();
 require("./config/cookieParserConfig")(app); //cookie parser
 app.use(limiter);
 app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "http://localhost:8080",
-      "https://mb-space-explorer.vercel.app",
-      "https://mb-space-explorer-dev.vercel.app",
-      "https://mb-portfolio.vercel.app",
-      "https://cook-blog-d3ed8.web.app",
-      "https://mb-cookblog.vercel.app",
-      "https://angular-cook-blog.web.app",
-      "https://destiny2-bgs.vercel.app",
-    ],
-    credentials: true,
-  })
+    cors({
+        origin: [
+            "http://localhost:3000",
+            "http://localhost:8080",
+            "https://mb-space-explorer.vercel.app",
+            "https://mb-space-explorer-dev.vercel.app",
+            "https://mb-portfolio.vercel.app",
+            "https://cook-blog-d3ed8.web.app",
+            "https://mb-cookblog.vercel.app",
+            "https://angular-cook-blog.web.app",
+            "https://destiny2-bgs.vercel.app",
+            "https://task-users-list.vercel.app",
+        ],
+        credentials: true,
+    })
 );
 app.use(auth); //auth middleware
 require("./config/expressConfig")(app); //express config
 app.use(errorHandler); //error handler
 
 app.listen(process.env.PORT, (error) => {
-  if (error) {
-    console.log(error.message);
-  }
-  console.log(
-    `"Cook-blog API" listening to port http://localhost:${process.env.PORT}`
-  );
-  console.log(
-    `Cook-blog React APP and REST API documentation is available at: https://github.com/SpooRe91/react-cook-server/blob/main/README.md`
-  );
+    if (error) {
+        console.log(error.message);
+    }
+    console.log(`"Cook-blog API" listening to port http://localhost:${process.env.PORT}`);
+    console.log(
+        `Cook-blog React APP and REST API documentation is available at: https://github.com/SpooRe91/react-cook-server/blob/main/README.md`
+    );
 });
